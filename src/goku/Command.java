ff7525f7a762c25824d83dc8960a205033cb7b7c
Ng Zhi An <ngzhian@gmail.com>
2014-02-22 00:38:16 +0800

    Command doesn't contain any logic, it just contains data, hence remove the need to test it.

--- src/goku/Command.java
+++ src/goku/Command.java
@@ -13,10 +13,21 @@ public final class Command {
     EARLIEST_DEADLINE_FIRST, HIGHEST_PRIORITY_FIRST
   }
 
-  private String source;
-  private Type type;
-  private Task task;
-  private SortOrder sortOrder;
+  private final String source;
+  private final Type type;
+  private final Task task;
+  private final SortOrder sortOrder;
+
+  public Command(String source, Type type, Task task) {
+    this(source, type, task, SortOrder.EARLIEST_DEADLINE_FIRST);
+  }
+
+  public Command(String source, Type type, Task task, SortOrder sortOrder) {
+    this.source = source;
+    this.type = type;
+    this.task = task;
+    this.sortOrder = sortOrder;
+  }
 
   public String getSource() {
     return source;
@@ -33,60 +44,4 @@ public final class Command {
   public Task getTask() {
     return task;
   }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  /*
-   * A builder for creating immutable command instances. The various setX
-   * methods permits setting the same fields multiple times. The actual Command
-   * built will only depend on the latest values of those fields.
-   */
-  public static class Builder {
-    enum POS {
-      SOURCE, TYPE, TITLE, DEADLINE, STARTDATE, ENDDATE
-    };
-
-    private static final int POS_SOURCE = 0;
-    private static final int POS_TYPE = 1;
-    private static final int POS_TASK = 2;
-    private static final int POS_SORT_ORDER = 3;
-
-    Object[] attributes;
-
-    public Builder() {
-      attributes = new Object[4];
-    }
-
-    public Builder setSource(String source) {
-      attributes[POS_SOURCE] = source;
-      return this;
-    }
-
-    public Builder setType(Type type) {
-      attributes[POS_TYPE] = type;
-      return this;
-    }
-
-    public Builder setTask(Task task) {
-      attributes[POS_TASK] = new Task(task);
-      return this;
-    }
-
-    public Builder setSortOrder(SortOrder order) {
-      attributes[POS_SORT_ORDER] = order;
-      return this;
-    }
-
-    public Command build() {
-      Command c = new Command();
-      c.source = (String) attributes[POS_SOURCE];
-      c.type = (Type) attributes[POS_TYPE];
-      c.task = (Task) attributes[POS_TASK];
-      c.sortOrder = (SortOrder) attributes[POS_SORT_ORDER];
-      return c;
-    }
-  }
-
 }
