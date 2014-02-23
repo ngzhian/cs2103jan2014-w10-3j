d7e678a4565bbb211944ee990b95f4325ac405de
Jonathan Chiam <jczjchiam@gmail.com>
2014-02-23 21:06:26 +0800

    Updated UserInterface interface

--- src/goku/clui/UserInterface.java
+++ src/goku/clui/UserInterface.java
@@ -1,5 +1,26 @@
 package goku.clui;
 
+import goku.Command;
+import goku.Result;
+
+/*
+ * This interface governs the interactions between the UI (be it CLI or GUI) and GokuMain.
+ * It acts as the parser from UI to Main and processes the result from Main to UI
+ */
 public interface UserInterface {
 
+	/**
+	 * Method: getUserInput
+	 * @param input string from UI
+	 * @return Command object
+	 */
+	Command getUserInput(String input);
+	
+	/**
+	 * Method: feedBack
+	 * @param result obtained from Main
+	 * @return string output to UI
+	 */
+	String feedBack(Result result);
+	
 }
