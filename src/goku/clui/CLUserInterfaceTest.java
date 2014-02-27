b86189fa73da1189ea1ea67856ac38c35c066950
Jonathan Chiam <jczjchiam@gmail.com>
2014-02-27 15:06:19 +0800

    Added empty test case for CLUserInterface

---
+++ src/goku/clui/CLUserInterfaceTest.java
@@ -0,0 +1,14 @@
+package goku.clui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CLUserInterfaceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
