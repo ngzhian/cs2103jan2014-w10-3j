36955af538079af95ea10f61728f1438984ba72d
jocelynongyt <jocelynongyt@gmail.com>
2014-02-27 05:32:30 +0000

    Removed different add methods. View completed tasks and incomplete tasks methods added.

--- src/goku/GOKU.java
+++ src/goku/GOKU.java
@@ -2,8 +2,6 @@ package goku;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
 
 /*
  * Task is the core of GOKU. GOKU is designed to keep track of tasks, which are
@@ -12,70 +10,56 @@ import java.util.Scanner;
 class GOKU {
   private static final String ADDED = "task added taskid: [id]";
   private static final String COMPLETE = "task [id] marked as completed";
-  private static final String DELETED = "task [id] deleted;
+  private static final String DELETED = "task [id] deleted";
   private static final String DISPLAYED = "%d. %s\n";
   private static final String EMPTY = "%s is empty\n";
-  
+
   private static GOKU goku;
-  
-  private static String addComplete(Task task)
-      throws IOException {
-    goku.completed.add(task);
+
+  public static void main(String[] args) throws IOException {
     
-    return String.format(COMPLETE);
+
   }
-  private static void addIncomplete(Task task)
-      throws IOException {
-    goku.incomplete.add(task);
-  }
-  private static String addNoDate(Task task)
-      throws IOException {
-    goku.noDate.add(task);
-    goku.allTasks.add(task);
-    
-    return String.format(ADDED);
-  }
-  private static String addWithDeadline(Task task)
-      throws IOException {
-    goku.withDeadline.add(task);
-    goku.allTasks.add(task);
-    
-    return String.format(ADDED);
-  }
-  
-  public static String addWithPeriod(Task task)
-      throws IOException {
-    goku.withPeriod.add(task);
-    goku.allTasks.add(task);
-    
-    return String.format(ADDED);
-  }
-  private ArrayList<Task> allTasks;  
-  
-  private ArrayList<Task> withDeadline;
-  
-  
-  private ArrayList<Task> withPeriod;
-  
-  private ArrayList<Task> noDate;
-  
-  private ArrayList<Task> completed;
-  
-  private ArrayList<Task> incomplete;
-  
-  public GOKU() {
-    goku = new GOKU();
+
+  private ArrayList<Task> allTasks;
+
+  public GOKU(Task task) {
+    goku = new GOKU(task);
     allTasks = new ArrayList<Task>();
-    withDeadline = new ArrayList<Task>();
-    withPeriod = new ArrayList<Task>();
-    noDate = new ArrayList<Task>();
+    allTasks.add(task);
+    System.out.println(ADDED);
+  }
+  
+  private ArrayList<Task> createCompleted(ArrayList<Task> taskList) {
+    ArrayList<Task> completed = new ArrayList<Task>();
+    for (Task task : taskList) {
+      if (task.getStatus() == true)
+        completed.add(task);
+    }
     
-    completed = new ArrayList<Task>();
-    incomplete = new ArrayList<Task>();
+    return completed;
+  }
+  
+  private ArrayList<Task> createIncomplete(ArrayList<Task> taskList) {
+    ArrayList<Task> incomplete = new ArrayList<Task>();
+    for (Task task : taskList) {
+      if (task.getStatus())
+        incomplete.add(task);
+    }
     
+    return incomplete;
   }
   
   
+  private void display(ArrayList<Task> taskList) {
+    for (int i = 0; i < taskList.size(); i++) {
+      System.out.printf(DISPLAYED, i + 1, taskList.get(i).getTitle());
+    }
+  }
+  
+
+
 }
+
 
 
