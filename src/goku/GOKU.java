5dc59c883197b99de82f45bef9bd0f2e43e02a6f
jocelynongyt <jocelynongyt@gmail.com>
2014-02-27 09:34:01 +0000

    Extracted add, delete, display, edit, search methods out

--- src/goku/GOKU.java
+++ src/goku/GOKU.java
@@ -8,144 +8,55 @@ import java.util.ArrayList;
  * analogous to real life tasks which the user wishes to note down.
  */
 class GOKU {
-  private static final String ADDED = "task added, taskid: [id]";
-  private static final String COMPLETE = "task [id] marked as completed";
-  private static final String DELETED = "task [id] deleted";
-  private static final String DISPLAYED = "%d. %s\n";
-  private static final String EMPTY = "%s is empty\n";
 
-  private static GOKU goku;
+  private static ArrayList<Task> allTasks = new ArrayList<Task>();
 
-  public static void main(String[] args) throws IOException {
+  private static Command cmd = new Command(null, null, null);
 
-  }
-
-  private ArrayList<Task> allTasks;
-
-  public GOKU(Task task) {
-    allTasks = new ArrayList<Task>();
-  }
-
-  private Result addTask(Task task) {
-    allTasks.add(task);
-
-    return new Result(true, ADDED, null, allTasks);
-  }
-
-  private Result deleteTask(Task task) {
-    for (int i = 0; i < allTasks.size(); i++) {
-      if (allTasks.get(i) == task) {
-        allTasks.remove(i);
-      }
-    }
-
-    return new Result(true, DELETED, null, allTasks);
-  }
-
-  private void displayAll(Command command) {
-    displayComplete(command);
-    displayIncomplete(command);
-  }
-
-  private Result displayComplete(Command command) {
-    ArrayList<Task> result = new ArrayList<Task>();
-    for (int i = 0; i < allTasks.size(); i++) {
-      if (allTasks.get(i).getStatus() == true) {
-        result.add(allTasks.get(i));
-      }
-    }
-
-    return new Result(true, null, null, result);
-  }
-
-  private Result displayIncomplete(Command command) {
-    ArrayList<Task> result = new ArrayList<Task>();
-    for (int i = 0; i < allTasks.size(); i++) {
-      if (allTasks.get(i).getStatus() == false) {
-        result.add(allTasks.get(i));
-      }
-    }
-
-    return new Result(true, null, null, result);
-  }
-
-  private void executeCommand(Command command) {
+  private static void executeCommand(Command command) {
     switch (command.getType()) {
 
       case ADD :
-        addTask(command.getTask());
+        Add add = new Add();
+        add.addTask(command.getTask());
         break;
       case DELETE :
-        deleteTask(command.getTask());
-        break;
-      case SEARCH :
-        if (command.getTask().getTitle() != null) {
-          searchTitle(command);
-        } else {
-          searchTag(command);
-        }
+        Delete delete = new Delete();
+        delete.deleteTask(command.getTask());
         break;
       case DISPLAY :
-
-    }
-  }
-
-  private Result searchTag(Command command) {
-    ArrayList<Task> result = new ArrayList<Task>();
-    String tempString = command.getTask().getTags().toString();
-    for (int i = 0; i < allTasks.size(); i++) {
-      String[] tempArray = allTasks.get(i).getTags();
-      for (String element : tempArray) {
-        if (element.contains(tempString)) {
-          result.add(allTasks.get(i));
-        }
-      }
-    }
-
-    return new Result(true, null, null, result);
-  }
-
-  private Result searchTitle(Command command) {
-    ArrayList<Task> result = new ArrayList<Task>();
-    for (int i = 0; i < allTasks.size(); i++) {
-      if (allTasks.get(i).getTitle().contains(command.getTask().getTitle())) {
-        result.add(allTasks.get(i));
-      }
-    }
-
-    return new Result(true, null, null, result);
-  }
-
-  private Result updateTask(Command command) {
-    int ID = command.getTask().getId();
-    ArrayList<Task> result = new ArrayList<Task>();
-    for (int i = 0; i < allTasks.size(); i++) {
-      if (allTasks.get(i).getId() == ID) {
-        result.add(allTasks.get(i));
-        if (command.getTask().getTitle() != null) {
-          allTasks.get(i).setTitle(command.getTask().getTitle());
-        }
+        Display display = new Display();
         if (command.getTask().getDeadline() != null) {
-          allTasks.get(i).setDeadline(command.getTask().getDeadline());
+          display.displayDate(command);
+        } else {
+          display.displayAll(command);
         }
-        if (command.getTask().getDateRange() != null) {
-          allTasks.get(i).setPeriod(command.getTask().getDateRange());
+        break;
+      case SEARCH :
+        Search search = new Search();
+        if (command.getTask().getTitle() != null) {
+          search.searchTitle(command);
+        } else {
+          search.searchTag(command);
         }
-        if (command.getTask().getTags() != null) {
-          allTasks.get(i).setTags(command.getTask().getTags());
-        }
-        if (command.getTask().getNotes() != null) {
-          allTasks.get(i).setNotes(command.getTask().getNotes());
-        }
-        if (command.getTask().getImportance() != null) {
-          allTasks.get(i).setImportance(command.getTask().getImportance());
-        }
-      }
-
+        break;
+      case EDIT :
+        Edit edit = new Edit();
+        edit.updateTask(command);
+        break;
     }
+  }
 
-    return new Result(true, null, null, result);
+  public static ArrayList<Task> getAllTasks() {
+    return allTasks;
+  }
 
+  public static void main(String[] args) throws IOException {
+    executeCommand(cmd);
+  }
+
+  public GOKU(Command command) {
+    cmd = command;
   }
 
 }
