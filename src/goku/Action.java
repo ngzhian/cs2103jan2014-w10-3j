786c82a88c715a4c7163751c27fdeb600b1dfcc1
Ng Zhi An <ngzhian@gmail.com>
2014-02-28 15:01:45 +0800

    Success and Error messages no longer defined in Action superclass, as different Actions will have different types and numbers of messages.

--- src/goku/Action.java
+++ src/goku/Action.java
@@ -1,19 +1,12 @@
 package goku;
 
 public abstract class Action {
+  GOKU goku;
   Command command;
   String SUCCESS_MSG;
   String FAILURE_MSG;
 
   abstract Result doIt();
-
-  String getSuccessMsg(Object... args) {
-    return String.format(SUCCESS_MSG, args);
-  }
-
-  String getErrorMsg(Object... args) {
-    return String.format(FAILURE_MSG, args);
-  }
 
   public void setCommand(Command command) {
     this.command = command;
