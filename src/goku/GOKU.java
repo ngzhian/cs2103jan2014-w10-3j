f7b658099bb51c4adab4e1114bf8a16f465ac35a
Ng Zhi An <ngzhian@gmail.com>
2014-03-01 17:04:40 +0800

    Encapsulate the implementation of a list of task.

--- src/goku/GOKU.java
+++ src/goku/GOKU.java
@@ -1,24 +1,21 @@
 package goku;
 
-import java.util.ArrayList;
-
 /*
  * Task is the core of GOKU. GOKU is designed to keep track of tasks, which are
  * analogous to real life tasks which the user wishes to note down.
  */
-class GOKU {
+public class GOKU {
 
-  private static ArrayList<Task> allTasks = new ArrayList<Task>();
-  private static TaskList _list = new TaskList();
+  private static TaskList _list;
+
+  public void setTaskList(TaskList list) {
+    _list = list;
+  }
 
   public Result executeCommand(Command command) {
     Action action = ActionFactory.buildAction(command);
     Result result = action.doIt();
     return result;
-  }
-
-  public static ArrayList<Task> getAllTasks() {
-    return allTasks;
   }
 
   public static TaskList getTaskList() {
