a2d844745361c9ba4acf05105bb01de3c4775f44
Ng Zhi An <ngzhian@gmail.com>
2014-03-01 11:17:41 +0800

    Modify tests to use System.lineSeparator() instead of hardcoded '\n'

--- src/goku/clui/Parser.java
+++ src/goku/clui/Parser.java
@@ -4,14 +4,17 @@ import goku.Task;
 
 /**
  * Parser interface to parse String input and create a relevant Task object
+ * 
  * @author jchiam
  **/
 public interface Parser {
 
-	/**
-	 * Method: parseString
-	 * @param input command string
-	 * @return Task object
-	 */
-	Task parseString (String input);
+  /**
+   * Method: parseString
+   * 
+   * @param input
+   *          command string
+   * @return Task object
+   */
+  Task parseString(String input);
 }
