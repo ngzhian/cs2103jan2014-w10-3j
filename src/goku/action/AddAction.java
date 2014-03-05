141119bdb5fea0d9299ddf65f147d2ae6ca551c9
Jocelyn <jocelynongyt@gmail.com>
2014-03-05 17:58:05 +0800

    edited to include undo action

--- src/goku/action/AddAction.java
+++ src/goku/action/AddAction.java
@@ -29,12 +29,42 @@ public class AddAction extends Action {
     list = goku.getTaskList();
   }
 
-  public String getTitle() {
-    return title;
+  private Result addTask() {
+    addToUndoList();
+    Task task = makeTask();
+    boolean success = list.addTask(task);
+    if (success) {
+      return successAddTask();
+    } else {
+      return failedToAddTask();
+    }
+  }
+
+  public void addToUndoList() {
+    TaskList beforeAddList = new TaskList();
+    for (Task t : list.getArrayList()) {
+      beforeAddList.addUndoTask(t);
+    }
+    goku.getUndoList().offer(beforeAddList);
+  }
+
+  @Override
+  public Result doIt() {
+    return addTask();
+  }
+
+  private Result failedToAddTask() {
+    Result result = Result.makeFailureResult();
+    result.setErrorMsg(String.format(ERR_FAIL, title));
+    return result;
   }
 
   public String getDeadline() {
     return deadline;
+  }
+
+  public String getTitle() {
+    return title;
   }
 
   private Task makeTask() {
@@ -45,31 +75,10 @@ public class AddAction extends Action {
     return task;
   }
 
-  private Result addTask() {
-    Task task = makeTask();
-    boolean success = list.addTask(task);
-    if (success) {
-      return successAddTask();
-    } else {
-      return failedToAddTask();
-    }
-  }
-
   private Result successAddTask() {
     Result result = Result.makeSuccessResult();
     result.setSuccessMsg(String.format(MSG_SUCCESS, title));
     result.setTasks(list);
     return result;
-  }
-
-  private Result failedToAddTask() {
-    Result result = Result.makeFailureResult();
-    result.setErrorMsg(String.format(ERR_FAIL, title));
-    return result;
-  }
-
-  @Override
-  public Result doIt() {
-    return addTask();
   }
 }
