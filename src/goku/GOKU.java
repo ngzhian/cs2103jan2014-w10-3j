59b19008601b594e25413e60bec20cbdfb2f5234
Jocelyn <jocelynongyt@gmail.com>
2014-03-05 18:14:42 +0800

    edited GOKU.java to include undoList

--- src/goku/GOKU.java
+++ src/goku/GOKU.java
@@ -1,5 +1,7 @@
 package goku;
 
+import java.util.LinkedList;
+
 /*
  * Task is the core of GOKU. GOKU is designed to keep track of tasks, which are
  * analogous to real life tasks which the user wishes to note down.
@@ -7,17 +9,23 @@ package goku;
 public class GOKU {
 
   private TaskList _list;
+  private LinkedList<TaskList> undoList;
 
   public GOKU() {
     _list = new TaskList();
-  }
-
-  public void setTaskList(TaskList list) {
-    _list = list;
+    undoList = new LinkedList<TaskList>();
   }
 
   public TaskList getTaskList() {
     return _list;
+  }
+
+  public LinkedList<TaskList> getUndoList() {
+    return undoList;
+  }
+
+  public void setTaskList(TaskList list) {
+    _list = list;
   }
 
 }
