8725b430361bc476e3c0b893a5bcff2eeb850049
Jocelyn <jocelynongyt@gmail.com>
2014-03-05 17:58:37 +0800

    edited to include undo action

--- src/goku/action/EditAction.java
+++ src/goku/action/EditAction.java
@@ -3,6 +3,8 @@ package goku.action;
 import goku.GOKU;
 import goku.Result;
 import goku.Task;
+import goku.TaskList;
+
 import java.util.Date;
 
 /*
@@ -10,32 +12,47 @@ import java.util.Date;
  * analogous to real life tasks which the user wishes to note down.
  */
 public class EditAction extends Action {
+  private final String MSG_SUCCESS = "edited task";
+
+  public int id;
+
+  public String deadline;
+
+  public String from;
+  public String to;
+  public String title;
+  public Date dline;
+
   public EditAction(GOKU goku) {
     super(goku);
     // TODO Auto-generated constructor stub
   }
 
-  private final String MSG_SUCCESS = "edited task";
+  public void addToUndoList() {
+    TaskList beforeAddList = new TaskList();
+    for (Task t : list.getArrayList()) {
+      beforeAddList.addUndoTask(t);
+    }
+    goku.getUndoList().offer(beforeAddList);
+  }
 
-  public int id;
-  public String deadline;
-  public String from;
-  public String to;
-  public String title;
-  public Date dline;
-  
+  @Override
+  public Result doIt() {
+    return updateTask();
+  }
+
   public Result updateTask() {
+    TaskList beforeAddList = new TaskList();
+    for (Task t : list.getArrayList()) {
+      beforeAddList.addUndoTask(new Task(t));
+    }
+    goku.getUndoList().offer(beforeAddList);
     Task taskWithEdits = new Task();
     taskWithEdits.setTitle(title);
     taskWithEdits.setDeadline(dline);
     Task t = list.getTaskById(id);
     t.updateWith(taskWithEdits);
     return new Result(true, MSG_SUCCESS, null, null);
-  }
-
-  @Override
-  public Result doIt() {
-    return updateTask();
   }
 
 }
