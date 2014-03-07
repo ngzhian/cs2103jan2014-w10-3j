c7f59e09a02ba05db24a2e20511346b0dfc4081d
Jonathan Chiam <jczjchiam@gmail.com>
2014-03-07 16:13:09 +0800

    Implemented search by deadline and period in parser

--- src/goku/action/SearchAction.java
+++ src/goku/action/SearchAction.java
@@ -1,29 +1,58 @@
 package goku.action;
 
+import java.util.Date;
+
+import goku.DateRange;
 import goku.GOKU;
 import goku.Result;
 import goku.Task;
 import goku.TaskList;
 
 public class SearchAction extends Action {
-  public SearchAction(GOKU goku) {
-    super(goku);
-    // TODO Auto-generated constructor stub
-  }
 
-  private static final String MSG_SUCCESS = "Found tasks!";
+	public String title;
+	public String deadline;
+	public String from;
+	public String to;
+	public Date dline;
+	public DateRange period;
 
-  public String title;
+	public SearchAction(GOKU goku) {
+		super(goku);
+		
+		// initialise params to null
+		deadline = null;
+		from = null;
+		to = null;
+		dline = null;
+		period = null;
+	}
 
-  public Result searchTitle() {
-    Task task = new Task();
-    task.setTitle(title);
-    TaskList foundTasks = list.findTaskByTitle(task);
-    return new Result(true, MSG_SUCCESS, null, foundTasks);
-  }
+	private static final String MSG_SUCCESS = "Found tasks!";
 
-  @Override
-  public Result doIt() {
-    return searchTitle();
-  }
+	public Result searchTitle() {
+		Task task = new Task();
+		task.setTitle(title);
+		TaskList foundTasks = list.findTaskByTitle(task);
+		return new Result(true, MSG_SUCCESS, null, foundTasks);
+	}
+	
+	public Result searchByDeadline() {
+		
+		
+		return new Result(true, MSG_SUCCESS, null, foundTasks);
+	}
+
+	@Override
+	public Result doIt() {
+		return searchTitle();
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public String getTitle() {
+		return title;
+	}
 }
