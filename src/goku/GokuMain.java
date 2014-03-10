98a53b354b173e8ddcdf2e53770bd1eedb6f4c27
Ng Zhi An <ngzhian@gmail.com>
2014-03-10 13:45:02 +0800

    Add feature to load stuff from default storage file

--- src/goku/GokuMain.java
+++ src/goku/GokuMain.java
@@ -4,27 +4,55 @@ import goku.ui.CLUserInterface;
 import goku.ui.GUserInterface;
 import goku.ui.UserInterface;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
 public class GokuMain {
-	static UserInterface ui;
+  static UserInterface ui;
+  static GOKU goku = new GOKU();
 
-	public static void main(String[] args) {
-		ui = setUserInterface(args);
-		ui.run();
-	}
+  public static void main(String[] args) {
+    try {
+      TaskList tasklist = tryToLoadFile();
+      goku.setTaskList(tasklist);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    ui = setUserInterface(args);
+    ui.run();
+  }
 
-	private static UserInterface setUserInterface(String[] args) {
-		if (shouldRunGui(args)) {
-			return new GUserInterface(new GOKU());
-		} else {
-			return new CLUserInterface(new GOKU());
-		}
-	}
+  private static TaskList tryToLoadFile() throws IOException {
+    TaskList tasklist = new TaskList();
+    try (BufferedReader br = new BufferedReader(new FileReader("store.goku"))) {
+      String line = br.readLine();
+      while (line != null) {
+        Gson gson = new Gson();
+        Task task = gson.fromJson(line, Task.class);
+        tasklist.addTaskWithoutSettingId(task);
+        line = br.readLine();
+      }
+    }
+    return tasklist;
+  }
 
-	private static boolean shouldRunGui(String[] args) {
-		if (args.length == 0) {
-			return false;
-		}
-		return args[0].equalsIgnoreCase("gui");
-	}
+  private static UserInterface setUserInterface(String[] args) {
+    if (shouldRunGui(args)) {
+      return new GUserInterface(goku);
+    } else {
+      return new CLUserInterface(goku);
+    }
+  }
+
+  private static boolean shouldRunGui(String[] args) {
+    if (args.length == 0) {
+      return false;
+    }
+    return args[0].equalsIgnoreCase("gui");
+  }
 
 }
--- src/goku/TaskList.java
+++ src/goku/TaskList.java
@@ -4,149 +4,156 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class TaskList implements Iterable<Task> {
-	private static Integer count = 0;
+  private static Integer count = 0;
 
-	private ArrayList<Task> _list;
+  private ArrayList<Task> _list;
 
-	public TaskList() {
-		_list = new ArrayList<Task>();
-	}
+  public TaskList() {
+    _list = new ArrayList<Task>();
+  }
 
-	public boolean addTask(Task task) {
-		task.setId(++count);
-		return _list.add(task);
-	}
-	
-	public boolean appendTask(Task task) {
-		return _list.add(task);
-	}
+  public boolean addTask(Task task) {
+    task.setId(++count);
+    return _list.add(task);
+  }
 
-	public void addUndoTask(Task task) {
-		_list.add(task);
-	}
+  public boolean addTaskWithoutSettingId(Task task) {
+    if (getTaskById(task.getId()) == null) {
+      return _list.add(task);
+    }
+    return false;
+  }
 
-	public void clear() {
-		_list.clear();
-	}
+  public boolean appendTask(Task task) {
+    return _list.add(task);
+  }
 
-	private TaskList deleteTask(TaskList matches) {
-		if (matches.size() == 1) {
-			deleteTaskById(matches.getTaskByIndex(0).getId());
-			return new TaskList();
-		} else {
-			return matches;
-		}
-	}
+  public void addUndoTask(Task task) {
+    _list.add(task);
+  }
 
-	public Task deleteTaskById(int id) {
-		int index = getIndexOfTaskById(id);
-		if (index < 0) {
-			return null;
-		} else {
-			return deleteTaskByIndex(index);
-		}
-	}
+  public void clear() {
+    _list.clear();
+  }
 
-	public Task deleteTaskByIndex(int index) {
-		return _list.remove(index);
-	}
+  private TaskList deleteTask(TaskList matches) {
+    if (matches.size() == 1) {
+      deleteTaskById(matches.getTaskByIndex(0).getId());
+      return new TaskList();
+    } else {
+      return matches;
+    }
+  }
 
-	public TaskList deleteTaskByTitle(Task toDelete) {
-		TaskList matches = findTaskByTitle(toDelete);
-		return deleteTask(matches);
-	}
+  public Task deleteTaskById(int id) {
+    int index = getIndexOfTaskById(id);
+    if (index < 0) {
+      return null;
+    } else {
+      return deleteTaskByIndex(index);
+    }
+  }
 
-	public TaskList findTaskByDeadline(Task toFind) {
-		TaskList matches = new TaskList();
-		for (Task task : _list) {
-			if (task.getDeadline()==null) {
-				continue;
-			}
-			if (task.isDueBefore(toFind)) {
-				matches.appendTask(task);
-			}
-		}
-		return matches;
+  public Task deleteTaskByIndex(int index) {
+    return _list.remove(index);
+  }
 
-	}
+  public TaskList deleteTaskByTitle(Task toDelete) {
+    TaskList matches = findTaskByTitle(toDelete);
+    return deleteTask(matches);
+  }
 
-	public TaskList findTaskByTags(Task toFind) {
-		TaskList matches = new TaskList();
-		for (Task task : _list) {
-			if (task.tagsMatch(toFind)) {
-				matches.appendTask(task);
-			}
-		}
-		return matches;
-	}
+  public TaskList findTaskByDeadline(Task toFind) {
+    TaskList matches = new TaskList();
+    for (Task task : _list) {
+      if (task.getDeadline() == null) {
+        continue;
+      }
+      if (task.isDueBefore(toFind)) {
+        matches.appendTask(task);
+      }
+    }
+    return matches;
 
-	public TaskList findTaskByTitle(Task toFind) {
-		TaskList matches = new TaskList();
-		for (Task task : _list) {
-			if (task.titleMatches(toFind)) {
-				matches.appendTask(task);
-			}
-		}
-		return matches;
-	}
+  }
 
-	public TaskList getAll() {
-		return this;
-	}
+  public TaskList findTaskByTags(Task toFind) {
+    TaskList matches = new TaskList();
+    for (Task task : _list) {
+      if (task.tagsMatch(toFind)) {
+        matches.appendTask(task);
+      }
+    }
+    return matches;
+  }
 
-	public TaskList getAllCompleted() {
-		TaskList result = new TaskList();
-		for (Task task : _list) {
-			if (task.getStatus()) {
-				result.addTask(task);
-			}
-		}
-		return result;
-	}
+  public TaskList findTaskByTitle(Task toFind) {
+    TaskList matches = new TaskList();
+    for (Task task : _list) {
+      if (task.titleMatches(toFind)) {
+        matches.appendTask(task);
+      }
+    }
+    return matches;
+  }
 
-	public TaskList getAllIncomplete() {
-		TaskList result = new TaskList();
-		for (Task task : _list) {
-			if (!task.getStatus()) {
-				result.addTask(task);
-			}
-		}
-		return result;
-	}
+  public TaskList getAll() {
+    return this;
+  }
 
-	public ArrayList<Task> getArrayList() {
-		return _list;
-	}
+  public TaskList getAllCompleted() {
+    TaskList result = new TaskList();
+    for (Task task : _list) {
+      if (task.getStatus()) {
+        result.addTask(task);
+      }
+    }
+    return result;
+  }
 
-	private int getIndexOfTaskById(int id) {
-		return _list.indexOf(getTaskById(id));
-	}
+  public TaskList getAllIncomplete() {
+    TaskList result = new TaskList();
+    for (Task task : _list) {
+      if (!task.getStatus()) {
+        result.addTask(task);
+      }
+    }
+    return result;
+  }
 
-	/*
-	 * @param id
-	 * 
-	 * @returns the task with specified id
-	 */
-	public Task getTaskById(int id) {
-		for (Task task : _list) {
-			if (task.getId() == id) {
-				return task;
-			}
-		}
-		return null;
-	}
+  public ArrayList<Task> getArrayList() {
+    return _list;
+  }
 
-	protected Task getTaskByIndex(int index) {
-		return _list.get(index);
-	}
+  private int getIndexOfTaskById(int id) {
+    return _list.indexOf(getTaskById(id));
+  }
 
-	@Override
-	public Iterator<Task> iterator() {
-		return _list.listIterator();
-	}
+  /*
+   * @param id
+   * 
+   * @returns the task with specified id
+   */
+  public Task getTaskById(int id) {
+    for (Task task : _list) {
+      if (task.getId() == id) {
+        return task;
+      }
+    }
+    return null;
+  }
 
-	public int size() {
-		return _list.size();
-	}
+  protected Task getTaskByIndex(int index) {
+    return _list.get(index);
+  }
+
+  @Override
+  public Iterator<Task> iterator() {
+    return _list.listIterator();
+  }
+
+  public int size() {
+    return _list.size();
+  }
 
 }
