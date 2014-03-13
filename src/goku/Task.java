b112f18bc0f274bcba8a4c05b3f2a1084737e018
Jocelyn <jocelynongyt@gmail.com>
2014-03-13 13:25:05 +0800

    edited toString() to append importance

--- src/goku/Task.java
+++ src/goku/Task.java
@@ -11,18 +11,14 @@ import com.google.gson.Gson;
 
 public class Task {
 
-  enum Importance {
-    HIGH, MEDIUM, LOW
-  }
-
   private Integer id;
   private String title;
   private Date deadline;
   private DateRange period;
   private String[] tags;
   private String notes;
-  private Importance importance;
   private Boolean isComplete;
+  private Boolean isImpt;
 
   public Task() {
   }
@@ -37,62 +33,8 @@ public class Task {
     period = task.period;
     tags = task.tags;
     notes = task.notes;
-    importance = task.importance;
     isComplete = false;
-  }
-
-  public boolean titleMatches(Task otherTask) {
-    if (getTitle() == null || otherTask.getTitle() == null) {
-      return false;
-    }
-    String aTitle = getTitle().toLowerCase();
-    String otherTitle = otherTask.getTitle().toLowerCase();
-    return aTitle.contains(otherTitle);
-  }
-
-  public boolean tagsMatch(Task otherTask) {
-    String[] otherTags = otherTask.getTags();
-    for (String otherTag : otherTags) {
-      for (String tag : tags) {
-        if (otherTag.equalsIgnoreCase(tag)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  public boolean isDueOn(Date date) {
-    return DateUtil.isEarlierOrOn(deadline, date);
-  }
-
-  public boolean isDueOn(Task task) {
-    return DateUtil.isEarlierOrOn(deadline, task.getDeadline());
-  } 
-  
-  public boolean isDueBefore(Date date) {
-    return DateUtil.isEarlierThan(deadline, date);
-  }
-
-  public boolean isDueBefore(Task task) {
-    return DateUtil.isEarlierThan(deadline, task.getDeadline());
-  }
-
-  public boolean inPeriod(Date date) {
-    return DateUtil.isEarlierThan(date, getEndDate())
-        && DateUtil.isLaterThan(date, getStartDate());
-  }
-
-  public void updateWith(Task otherTask) {
-    title = otherTask.title == null ? title : otherTask.title;
-    deadline = otherTask.deadline == null ? deadline : otherTask.deadline;
-    period = otherTask.period == null ? period : otherTask.period;
-    isComplete = otherTask.isComplete == null ? isComplete
-        : otherTask.isComplete;
-    tags = otherTask.tags == null ? tags : otherTask.tags;
-    notes = otherTask.notes == null ? notes : otherTask.notes;
-    importance = otherTask.importance == null ? importance
-        : otherTask.importance;
+    isImpt = task.isImpt;
   }
 
   @Override
@@ -123,8 +65,8 @@ public class Task {
     return id;
   }
 
-  public Importance getImportance() {
-    return importance;
+  public Boolean getImpt() {
+    return isImpt;
   }
 
   public String getNotes() {
@@ -147,20 +89,45 @@ public class Task {
     return title;
   }
 
+  public boolean inPeriod(Date date) {
+    return DateUtil.isEarlierThan(date, getEndDate())
+        && DateUtil.isLaterThan(date, getStartDate());
+  }
+
   public Boolean isDone() {
     return isComplete;
   }
 
-  public void setDeadline(Date deadline) {
-    this.deadline = deadline;
+  public boolean isDueBefore(Date date) {
+    return DateUtil.isEarlierThan(deadline, date);
+  }
+
+  public boolean isDueBefore(Task task) {
+    return DateUtil.isEarlierThan(deadline, task.getDeadline());
+  }
+
+  public boolean isDueOn(Date date) {
+    return DateUtil.isEarlierOrOn(deadline, date);
+  }
+
+  public boolean isDueOn(Task task) {
+    return DateUtil.isEarlierOrOn(deadline, task.getDeadline());
   }
 
   public void setComplete(Boolean complete) {
     this.isComplete = complete;
   }
 
-  public void setImportance(Importance importance) {
-    this.importance = importance;
+  public void setDeadline(Date deadline) {
+    this.deadline = deadline;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setImpt(Boolean impt) {
+    this.isImpt = impt;
   }
 
   public void setNotes(String notes) {
@@ -187,8 +154,31 @@ public class Task {
     this.title = title;
   }
 
-  public void setId(int id) {
-    this.id = id;
+  public boolean tagsMatch(Task otherTask) {
+    String[] otherTags = otherTask.getTags();
+    for (String otherTag : otherTags) {
+      for (String tag : tags) {
+        if (otherTag.equalsIgnoreCase(tag)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public boolean titleMatches(Task otherTask) {
+    if (getTitle() == null || otherTask.getTitle() == null) {
+      return false;
+    }
+    String aTitle = getTitle().toLowerCase();
+    String otherTitle = otherTask.getTitle().toLowerCase();
+    return aTitle.contains(otherTitle);
+  }
+
+  public String toStorageFormat() {
+    Gson gson = new Gson();
+
+    return gson.toJson(this);
   }
 
   @Override
@@ -218,12 +208,25 @@ public class Task {
       sb.append("not done");
     }
 
+    sb.append(" | Importance: ");
+    if (isImpt != null && isImpt) {
+      sb.append("HIGH");
+    } else {
+      sb.append("NORMAL");
+    }
+
     return sb.toString();
+
   }
 
-  public String toStorageFormat() {
-    Gson gson = new Gson();
-
-    return gson.toJson(this);
+  public void updateWith(Task otherTask) {
+    title = otherTask.title == null ? title : otherTask.title;
+    deadline = otherTask.deadline == null ? deadline : otherTask.deadline;
+    period = otherTask.period == null ? period : otherTask.period;
+    isComplete = otherTask.isComplete == null ? isComplete
+        : otherTask.isComplete;
+    tags = otherTask.tags == null ? tags : otherTask.tags;
+    notes = otherTask.notes == null ? notes : otherTask.notes;
+    isImpt = otherTask.isImpt == null ? isImpt : otherTask.isImpt;
   }
 }
