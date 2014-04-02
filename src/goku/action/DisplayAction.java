ab3a4eb83adc9d09ac3233e493be14521200ffd9
Jocelyn <jocelynongyt@gmail.com>
2014-04-02 14:54:12 +0800

    Added msg when there are no complete tasks, no incomplete tasks and no overdue tasks available

--- src/goku/action/DisplayAction.java
+++ src/goku/action/DisplayAction.java
@@ -11,7 +11,10 @@ public class DisplayAction extends Action {
 
   private static final String MSG_SUCCESS = "Here are your tasks!";
   private static final String MSG_SUCCESS_OVERDUE = "Here are your overdue tasks!";
-  private static final String MSG_HAS_OVERDUE = "You have overdue tasks, \"view overdue\" to see them.";
+  private static final String MSG_HAS_OVERDUE = "[!] You have overdue tasks, \"view overdue\" to see them.";
+  private static final String MSG_NO_COMPLETED = "You have not completed any tasks!\nTry \"done [ID]\" to mark a task as completed!";
+  private static final String MSG_NO_INCOMPLETE = "There is currently no upcoming tasks!\nTry \"add [TITLE] by [TIME] [DAY]\" or\n\"add [TITLE] from [TIME] [DAY] to [TIME] [DAY]\" to add a task now!\n";
+  private static final String MSG_NO_OVERDUE = "No overdue tasks!";
   DateTime byDeadline;
   public boolean viewComplete;
   public boolean viewOverdue;
@@ -21,12 +24,12 @@ public class DisplayAction extends Action {
     shouldSaveAfter = false;
   }
 
-  public Result displayOverdue() {
-    return new Result(true, MSG_SUCCESS_OVERDUE, null, list.getOverdue());
-  }
-
   public Result displayComplete() {
-    return new Result(true, MSG_SUCCESS, null, list.getAllCompleted());
+    if (list.getAllCompleted().isEmpty()) {
+      return new Result(false, null, MSG_NO_COMPLETED, null);
+    } else {
+      return new Result(true, MSG_SUCCESS, null, list.getAllCompleted());
+    }
   }
 
   public Result displayDate() {
@@ -39,11 +42,21 @@ public class DisplayAction extends Action {
   }
 
   public Result displayIncomplete() {
-    String message = MSG_SUCCESS;
-    if (list.getOverdue().size() != 0) {
-      message += System.lineSeparator() + MSG_HAS_OVERDUE;
+    if (list.getAllIncomplete().isEmpty()) {
+      return new Result(true, editMsgIfHaveOverdue(MSG_NO_INCOMPLETE), null,
+          null);
+    } else {
+      return new Result(true, editMsgIfHaveOverdue(MSG_SUCCESS), null,
+          list.getAllIncomplete());
     }
-    return new Result(true, message, null, list.getAllIncomplete());
+  }
+
+  public Result displayOverdue() {
+    if (list.getOverdue().isEmpty()) {
+      return new Result(true, MSG_NO_OVERDUE, null, null);
+    } else {
+      return new Result(true, MSG_SUCCESS_OVERDUE, null, list.getOverdue());
+    }
   }
 
   @Override
@@ -55,6 +68,13 @@ public class DisplayAction extends Action {
     } else {
       return displayIncomplete();
     }
+  }
+
+  public String editMsgIfHaveOverdue(String msg) {
+    if (list.getOverdue().size() != 0) {
+      msg += System.lineSeparator() + MSG_HAS_OVERDUE;
+    }
+    return msg;
   }
 
 }
