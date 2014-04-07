3893eb7525a12e762a3aac4d6485b5076c1786d7
Ng Zhi An <ngzhian@gmail.com>
2014-04-07 11:52:18 +0800

    Fixes 115. Fix how intersectsWith works for DateRange. Previously we did not check when one DateRange was completely in another DateRange, this is now added as an OR clause.

--- src/goku/DateRange.java
+++ src/goku/DateRange.java
@@ -8,14 +8,15 @@ import hirondelle.date4j.DateTime;
 public class DateRange {
   DateTime startDate;
   DateTime endDate;
-  
+
   private static final String ERR_INVALID_PERIOD = "Invalid period detected!";
-  
-  public DateRange(DateTime startDate, DateTime endDate) throws InvalidDateRangeException{
+
+  public DateRange(DateTime startDate, DateTime endDate)
+      throws InvalidDateRangeException {
     this.startDate = startDate;
     this.endDate = endDate;
-    
-    if(DateUtil.isEarlierOrOn(endDate, startDate)) {
+
+    if (DateUtil.isEarlierOrOn(endDate, startDate)) {
       throw new InvalidDateRangeException(ERR_INVALID_PERIOD);
     }
   }
@@ -27,21 +28,31 @@ public class DateRange {
   public DateTime getEndDate() {
     return endDate;
   }
-  
+
   public boolean containsDate(DateTime date) {
-    if (date == null) return false;
+    if (date == null) {
+      return false;
+    }
     return DateUtil.isEarlierOrOn(date, getEndDate())
         && DateUtil.isLaterOrOn(date, getStartDate());
   }
-  
+
   public boolean intersectsWith(DateRange range) {
-    if (range == null) return false;
-    return containsDate(range.getStartDate()) || containsDate(range.getEndDate());
+    if (range == null) {
+      return false;
+    }
+    return containsDate(range.getStartDate())
+        || containsDate(range.getEndDate()) || isContainedIn(range);
   }
-  
+
+  private boolean isContainedIn(DateRange range) {
+    return startDate.gteq(range.getStartDate())
+        && endDate.lteq(range.getEndDate());
+  }
+
   @Override
   public String toString() {
-    return DateOutput.formatDateTimeDayMonthHourMin(startDate)+" to "+
-        DateOutput.formatDateTimeDayMonthHourMin(endDate);
+    return DateOutput.formatDateTimeDayMonthHourMin(startDate) + " to "
+        + DateOutput.formatDateTimeDayMonthHourMin(endDate);
   }
 }
