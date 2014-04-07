3899aa8d847e0badc13c992b0ea42cf38494380a
Jocelyn <jocelynongyt@gmail.com>
2014-04-07 19:43:03 +0800

    edited getAllIncomplete and getOverdue to include period tasks that are incomplete/overdue also improved isToday(DateRange daterange)

---
+++ src/goku/action/SearchAction.java
@@ -0,0 +1,212 @@
+package goku.action;
+
+import goku.DateRange;
+import goku.GOKU;
+import goku.Result;
+import goku.Task;
+import goku.TaskList;
+import goku.util.DateUtil;
+import goku.util.InvalidDateRangeException;
+import hirondelle.date4j.DateTime;
+
+import java.util.List;
+
+public class SearchAction extends Action {
+
+  public String title;
+  public String deadline;
+  public String from;
+  public String to;
+  public DateTime dline;
+  public DateRange period;
+  public DateTime onDateQuery;
+  public boolean testFree = false;
+  public DateTime freeDateQuery;
+
+  private static final String MSG_SUCCESS = "Found tasks for \"%s\"...";
+
+  private static final String MSG_FAIL_BY_TITLE = "No relevant tasks for \"%s\".";
+  private static final String MSG_FAIL = "No relevant tasks.";
+  private static final String IS_FREE = "Specified datetime is available.";
+  private static final String NOT_FREE = "Specified datetime is not available.";
+  private static final String FREE_SLOTS = "Here are your free timings of the day.";
+  private static final String NO_FREE_SLOTS = "Your day is fully packed!";
+  public static final String ERR_INSUFFICIENT_ARGS = "Can't search! Try \"search title\"";
+  public static final String ERR_NO_VALID_DATE_FOUND = "Can't search! Try entering a valid date after \"free\"";
+  private static final String ERR_DEADLINE_PERIOD_CONFLICT = "Can't search! Conflicting deadline and period.";
+  private static final String MSG_HAS_OVERDUE = "[!] You have overdue tasks, \"view overdue\" to see them.";
+
+  public SearchAction(GOKU goku) {
+    super(goku);
+    shouldSaveAfter = false;
+  }
+
+  /*
+   * Case 1: dateQuery contains specific date and time Checks whether dateQuery
+   * contains any tasks whose period coincides If there is => not free, else =>
+   * free
+   * 
+   * Case 2: dateQuery only contains date Shows users the list of free(?)
+   * timeslots of the specified day
+   */
+  public Result checkFreeTime() throws InvalidDateRangeException {
+    assert (freeDateQuery != null);
+
+    // Case 1: Specific date and time
+    if (freeDateQuery.getHour() != null) {
+      return checkIfFree();
+    } else { // Case 2: Only date given
+      return checkFreeSlots();
+    }
+  }
+
+  private Result checkIfFree() {
+    if (list.isFree(freeDateQuery) == true) {
+      return new Result(true, IS_FREE, null, null);
+    } else {
+      return new Result(false, null, NOT_FREE, null);
+    }
+  }
+  
+  private Result checkFreeSlots() throws InvalidDateRangeException {
+    assert freeDateQuery.getHour() == null;
+    
+    List<String> freeSlots = list.findFreeSlots(freeDateQuery);
+    
+    if (!freeSlots.isEmpty()) {
+      return new Result(true, FREE_SLOTS, null, freeSlots, null);
+    } else {
+      return new Result(false, null, NO_FREE_SLOTS, null);
+    }
+  }
+
+  @Override
+  public Result doIt() {
+
+    Result result = null;
+
+    if (freeDateQuery != null) {
+      try {
+        result = checkFreeTime();
+      } catch (InvalidDateRangeException e) {
+        e.printStackTrace();
+      }
+    } else if (onDateQuery != null) {
+      result = searchTasksOnDay();
+    } else if (dline != null && period != null) {
+      result = searchByDeadlineInPeriod();
+    } else if (dline != null) {
+      result = searchByDeadline();
+    } else if (period != null) {
+      result = searchByPeriod();
+    } else {
+      result = searchTitle();
+    }
+
+    return result;
+  }
+
+  public String editMsgIfHaveOverdue(String msg) {
+    if (list.getOverdue().size() != 0) {
+      msg += System.lineSeparator() + MSG_HAS_OVERDUE;
+    }
+    return msg;
+  }
+
+  public String getDeadline() {
+    return deadline;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+  
+  public Result searchTasksOnDay() {
+    List<Task> foundTasks = list.findTasksOnDay(onDateQuery);
+    if (foundTasks.size() != 0) {
+      return new Result(true, String.format(MSG_SUCCESS, DateUtil.toString(onDateQuery)), null, foundTasks);
+    } else {
+      return new Result(false, null, editMsgIfHaveOverdue(String.format(MSG_FAIL, DateUtil.toString(onDateQuery))), null);
+    }
+  }
+
+  public Result searchByDeadline() {
+    Task task = new Task();
+
+    assert (dline != null);
+    task.setDeadline(dline);
+    List<Task> foundTasks = list.findTaskByDeadline(dline);
+    if (foundTasks.size() != 0) {
+      return new Result(true, String.format(MSG_SUCCESS,
+          DateUtil.toString(dline)), null, foundTasks);
+    } else {
+      return new Result(false, null, editMsgIfHaveOverdue(String.format(
+          MSG_FAIL, DateUtil.toString(dline))), null);
+    }
+  }
+
+  /*
+   * Searches for tasks within a given period that has the specified deadline.
+   */
+  private Result searchByDeadlineInPeriod() {
+    // check for conflicting deadline and period
+    if (DateUtil.isEarlierThan(dline, period.getStartDate())
+        || DateUtil.isLaterThan(dline, period.getEndDate())) {
+      return new Result(false, null, ERR_DEADLINE_PERIOD_CONFLICT, null);
+    }
+
+    Result byPeriod = searchByPeriod();
+    TaskList tasksDueInPeriod = new TaskList();
+
+    if (byPeriod.getTasks() != null) {
+      for (Task task : byPeriod.getTasks()) {
+        if (task.getDeadline()!=null && DateUtil.isEarlierOrOn(task.getDeadline(), dline)) {
+          tasksDueInPeriod.addTaskWithoutSettingId(task);
+        }
+      }
+
+      return new Result(true, String.format(MSG_SUCCESS, period.toString()),
+          null, tasksDueInPeriod.asList());
+    } else {
+      return new Result(false, null, editMsgIfHaveOverdue(MSG_FAIL), null);
+    }
+  }
+
+  /*
+   * Searches for tasks that fall within a specified period. Tasks that
+   * start/end, or has deadline within the period is a match. Example: --- Time
+   * line --------------------------------> 1 2 3 4 5 6 7 8 9 10 11
+   * ########################## | |-----| |---| | |------| | A B C D E F
+   * 
+   * | - a task with a deadline at that point in time |-----| a task with a
+   * period starting at the first | and ending at second | ### - the period we
+   * wish to query for Search should then return tasks, B, C, D, E
+   */
+  public Result searchByPeriod() {
+    Task task = new Task();
+
+    assert (period != null);
+
+    task.setPeriod(period);
+    List<Task> foundTasks = list.findTaskByPeriod(period);
+    if (foundTasks.size() != 0) {
+      return new Result(true, String.format(MSG_SUCCESS, period), null,
+          foundTasks);
+    } else {
+      return new Result(false, null, editMsgIfHaveOverdue(MSG_FAIL), null);
+    }
+  }
+
+  public Result searchTitle() {
+    Task task = new Task();
+    task.setTitle(title);
+    List<Task> foundTasks = list.findTaskByTitle(title);
+    if (foundTasks.size() == 0) {
+      return new Result(false, null, editMsgIfHaveOverdue(String.format(
+          MSG_FAIL_BY_TITLE, title)), null);
+    } else {
+      return new Result(true, String.format(MSG_SUCCESS, title), null,
+          foundTasks);
+    }
+  }
+}
---
+++ src/goku/ui/GokuController.java
@@ -0,0 +1,268 @@
+package goku.ui;
+
+import goku.DateRange;
+import goku.GOKU;
+import goku.Result;
+import goku.Task;
+import goku.action.Action;
+import goku.action.ExitAction;
+import goku.action.MakeActionException;
+import goku.action.SearchAction;
+import goku.storage.LoadTasksException;
+import goku.storage.Storage;
+import goku.storage.StorageFactory;
+import goku.util.DateUtil;
+import goku.util.InvalidDateRangeException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GokuController {
+
+  private static final String ERR_PARSE_FILE_FAIL = "Error parsing JSON, loaded tasks to the best of my ability.";
+  private static final String ERR_LOAD_FILE_FAIL = "Error loading file, no tasks loaded.";
+  private static final String MSG_NEW_USER = "Seems like you're new!\nA file called \"store.goku\" has been created to save your tasks!\nType \"help\" to get a quick guide.";
+  private static final String MSG_FILE_NOT_FOUND = "File cannot be found, no tasks loaded.";
+  /* JavaFX scene objects */
+  @FXML
+  private ResourceBundle resources;
+  @FXML
+  private URL location;
+  @FXML
+  private AnchorPane page;
+  @FXML
+  private HBox titleBar;
+  @FXML
+  private Label minimizeButton;
+  @FXML
+  private Label closeButton;
+  @FXML
+  private ScrollPane scrollPane;
+  @FXML
+  private TextField inputField;
+  @FXML
+  private StackPane suggestionBox;
+  @FXML
+  private VBox suggestionList;
+
+  /* used for windows dragging */
+  private static double mousePressX;
+  private static double mousePressY;
+
+  private GOKU goku;
+  private InputParser parser;
+  private Storage storage;
+  private static final Logger LOGGER = Logger
+      .getLogger(Logger.GLOBAL_LOGGER_NAME);
+  private CompletionController completionController;
+  private FeedbackController feedbackController;
+  private HistoryController historyController;
+
+  @FXML
+  void initialize() {
+    goku = FXGUI.getGokuInstance();
+    completionController = new CompletionController(inputField, suggestionBox,
+        suggestionList);
+    feedbackController = new FeedbackController(scrollPane);
+    historyController = new HistoryController(inputField);
+    parser = new InputParser(goku);
+    storage = StorageFactory.getDefaultStorage();
+
+    // this listeners for tasks added to the list and extracts the title
+    // so that they can be autocompleted next time
+    goku.getObservable().addListener(
+        completionController.getCompletionListener());
+
+    loadSavedFiled();
+  }
+
+  private void loadSavedFiled() {
+    try {
+      goku.addToTaskList(storage.loadStorage());
+      doAction(new GreetAction(goku));
+    } catch (FileNotFoundException e) {
+      LOGGER.warning(MSG_FILE_NOT_FOUND);
+      feedbackController.displayLine(MSG_NEW_USER);
+    } catch (IOException e) {
+      LOGGER.warning(ERR_LOAD_FILE_FAIL);
+      feedbackController.displayErrorMessage(ERR_LOAD_FILE_FAIL);
+    } catch (LoadTasksException e) {
+      LOGGER.warning(ERR_PARSE_FILE_FAIL);
+      feedbackController.displayErrorMessage(e.getMessage());
+      goku.addToTaskList(e.getLoadedTasks());
+    }
+  }
+
+  /*
+   * This method is called when a keypress in inputField is detected. A "Enter"
+   * keypress means the user wants to run the command. Any other key will be
+   * handled by CompletionController, which will suggestion completions. User
+   * can then use tab to *select* a completion and have it fill up the
+   * inputField. Ctrl + Z and Ctrl + Y is a shortcut for Undo and Redo,
+   * respectively.
+   */
+  public void keyPressOnInputField(KeyEvent event)
+      throws InvalidDateRangeException {
+    if (event.isControlDown()) {
+      handleControlKeypress(event);
+    } else if (event.getCode() == KeyCode.ENTER) {
+      commitInput();
+    } else if (isHistoryKey(event)) {
+      historyController.handle(event);
+    } else {
+      completionController.handle(event);
+    }
+  }
+
+  /*
+   * Called when user presses a Ctrl + ? combination,
+   * where ? is any key.
+   */
+  private void handleControlKeypress(KeyEvent event)
+      throws InvalidDateRangeException {
+    if (event.getCode() == KeyCode.Z) {
+      inputField.setText("undo");
+      commitInput();
+    } else if (event.getCode() == KeyCode.Y) {
+      inputField.setText("redo");
+      commitInput();
+    }
+  }
+
+  private boolean isHistoryKey(KeyEvent event) {
+    return event.getCode() == KeyCode.UP || event.getCode() == KeyCode.DOWN;
+  }
+
+  /*
+   * Called when the user clicks on the title bar,
+   * store the position of the mouse to allow for moving the entire window.
+   */
+  public void onTitleBarMousePress(MouseEvent event) {
+    mousePressX = event.getSceneX();
+    mousePressY = event.getSceneY();
+  }
+
+  /*
+   * Called when user drags on the title bar,
+   * then moves according to where the user drags
+   */
+  public void onTitleBarDrag(MouseEvent event) {
+    Stage stage = FXGUI.getStage();
+    stage.setX(event.getScreenX() - mousePressX);
+    stage.setY(event.getScreenY() - mousePressY);
+  }
+
+  public void exitButtonPress(MouseEvent event) {
+    Platform.exit();
+  }
+
+  public void minimizeButtonPress(MouseEvent event) {
+    Stage stage = FXGUI.getStage();
+    stage.setIconified(true);
+  }
+
+  private void commitInput() throws InvalidDateRangeException {
+    if (inputField.getText().isEmpty()) {
+      return;
+    }
+    try {
+      historyController.addInput(inputField.getText());
+      Action action = parser.parse(getUserInput());
+      if (action instanceof ExitAction) {
+        feedbackController.sayGoodbye();
+        Platform.exit();
+      }
+      doAction(action);
+    } catch (MakeActionException e) {
+      feedbackController.displayErrorMessage(e.getMessage());
+    }
+    clearInput();
+    hideSuggestions();
+  }
+
+  /*
+   * Retrieves user's input from the TextField
+   */
+  private String getUserInput() {
+    return inputField.getText().toLowerCase().trim();
+  }
+
+  /*
+   * Clears the TextField where user can enter input
+   */
+  private void clearInput() {
+    inputField.clear();
+  }
+
+  private void doAction(Action action) {
+    Result result = action.doIt();
+    feedBack(result);
+    if (action.shouldSave()) {
+      save();
+    }
+  }
+
+  /*
+   * Informs the user of a result of doing an action
+   */
+  private void feedBack(Result result) {
+    if (result == null) {
+      return;
+    } else {
+      feedbackController.displayResult(result);
+    }
+  }
+
+  private void hideSuggestions() {
+    suggestionBox.setVisible(false);
+  }
+
+  public void save() {
+    try {
+      storage.saveAll(goku.getTaskList());
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Error saving tasks.");
+    }
+  }
+
+  private class GreetAction extends Action {
+    private static final String MSG = "Welcome to GOKU!\nHere are your tasks for today!";
+
+    public GreetAction(GOKU goku) {
+      super(goku);
+    }
+
+    @Override
+    public Result doIt() {
+      SearchAction sa = new SearchAction(goku);
+      try {
+        sa.period = new DateRange(DateUtil.getNow(), DateUtil.getNow()
+            .getEndOfDay());
+      } catch (InvalidDateRangeException e) {
+      }
+      Result result2 = sa.doIt();
+      List<Task> list2 = result2.getTasks();
+
+      return new Result(true, MSG, null, list2);
+    }
+  }
+}
