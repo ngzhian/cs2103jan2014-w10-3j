7866528e8da816d27573ce86ef05c6e19bdcf6fe
Jocelyn <jocelynongyt@gmail.com>
2014-04-08 19:36:14 +0800

    fixed issue 124. (problem with changing id of completed task)

---
+++ src/goku/ui/LiveSearch.java
@@ -0,0 +1,71 @@
+package goku.ui;
+
+import goku.Commands;
+import goku.GOKU;
+import goku.Result;
+import goku.action.Action;
+import goku.action.MakeActionException;
+import goku.action.SearchAction;
+import goku.util.InvalidDateRangeException;
+
+import java.util.Arrays;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Provides live search capability to GOKU's GUI. LiveSearch handles every key
+ * press, but only reacts when the user is in a searching context. It identifies
+ * the context by looking at the first word in the input field. If that first
+ * word is part of a keyword that results in a SearchAction, it will then
+ * greedily parse the text in the input field and show the search results to the
+ * user.
+ * 
+ * @author ZhiAn
+ * 
+ */
+public class LiveSearch extends Controller {
+  private GOKU goku;
+  private TextField input;
+  private FeedbackPane feedback;
+  public StringBuilder query;
+
+  public LiveSearch(GOKU goku, TextField inputField, FeedbackPane feedbackPane) {
+    this.goku = goku;
+    this.input = inputField;
+    this.feedback = feedbackPane;
+  }
+
+  void search(String command) {
+    InputParser parser = new InputParser(goku);
+    Action a;
+    try {
+      a = parser.parse(command);
+    } catch (MakeActionException e) {
+      return;
+    } catch (InvalidDateRangeException e) {
+      return;
+    }
+    if (!(a instanceof SearchAction)) {
+      return;
+    }
+    SearchAction sa = (SearchAction) a;
+    Result r = sa.doIt();
+    feedback.displayResult(r);
+  }
+
+  @Override
+  boolean isHandling(KeyEvent key) {
+    return true;
+  }
+
+  @Override
+  void handle(KeyEvent key) {
+    String[] tokens = input.getText().split(" ");
+    if (tokens.length > 0
+        && Arrays.asList(Commands.searchKeywords).contains(tokens[0])) {
+      search(input.getText());
+    }
+
+  }
+}
---
+++ src/goku/util/InvalidDateRangeException.java
@@ -0,0 +1,9 @@
+package goku.util;
+
+@SuppressWarnings("serial")
+public class InvalidDateRangeException extends Exception {
+  
+  public InvalidDateRangeException(String message) {
+      super(message);
+  }
+}
