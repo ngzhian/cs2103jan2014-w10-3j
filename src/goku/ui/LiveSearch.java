bbac71f07722f247a4008b0f277ebaebd5d8d04d
Ng Zhi An <ngzhian@gmail.com>
2014-04-08 12:57:51 +0800

    Move LiveSearch to its own Controller.

--- src/goku/ui/LiveSearch.java
+++ src/goku/ui/LiveSearch.java
@@ -1,5 +1,6 @@
 package goku.ui;
 
+import goku.Commands;
 import goku.GOKU;
 import goku.Result;
 import goku.action.Action;
@@ -7,13 +8,31 @@ import goku.action.MakeActionException;
 import goku.action.SearchAction;
 import goku.util.InvalidDateRangeException;
 
-public class LiveSearch {
+import java.util.Arrays;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Provides live search capability to GOKU's GUI. LiveSearch handles every key
+ * press, but only reacts when the user is in a searching context. It identifies
+ * the context by looking at the first word in the input field. If that first
+ * word is part of a keyword that results in a SearchAction, it will then
+ * greedily parse the text in the input field and show the search results to the
+ * user.
+ * 
+ * @author ZhiAn
+ * 
+ */
+public class LiveSearch extends Controller {
   private GOKU goku;
+  private TextField input;
   private FeedbackPane feedback;
   public StringBuilder query;
 
-  public LiveSearch(GOKU goku, FeedbackPane feedbackPane) {
+  public LiveSearch(GOKU goku, TextField inputField, FeedbackPane feedbackPane) {
     this.goku = goku;
+    this.input = inputField;
     this.feedback = feedbackPane;
   }
 
@@ -33,5 +52,20 @@ public class LiveSearch {
     SearchAction sa = (SearchAction) a;
     Result r = sa.doIt();
     feedback.displayResult(r);
+  }
+
+  @Override
+  boolean isHandling(KeyEvent key) {
+    return true;
+  }
+
+  @Override
+  void handle(KeyEvent key) {
+    String[] tokens = input.getText().split(" ");
+    if (tokens.length > 0
+        && Arrays.asList(Commands.searchKeywords).contains(tokens[0])) {
+      search(input.getText());
+    }
+
   }
 }
