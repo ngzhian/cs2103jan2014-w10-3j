94b5da7d1d4e86fed2b53a3b94835663e778f81f
soundaholic92 <soundaholic92@localhost>
2014-04-13 10:50:47 +0800

    Collated action tests

---
+++ src/goku/action/RedoAction.java
@@ -0,0 +1,58 @@
+package goku.action;
+
+import goku.GOKU;
+import goku.Result;
+import goku.TaskList;
+
+public class RedoAction extends Action {
+  private static final String MSG_SUCCESS = "Command executed: \"%s\"";
+  private static final String ERR_FAIL = "Failed to redo.";
+  private static final String MSG_HAS_OVERDUE = "[!] You have overdue tasks, \"view overdue\" to see them.";
+
+  private boolean isEmpty = false;
+
+  public RedoAction(GOKU goku) {
+    super(goku);
+    list = goku.getTaskList();
+    // TODO Auto-generated constructor stub
+  }
+
+  public String editMsgIfHaveOverdue(String msg) {
+    if (goku.getTaskList().getOverdue().size() != 0) {
+      msg += System.lineSeparator() + MSG_HAS_OVERDUE;
+    }
+    return msg;
+  }
+
+  @Override
+  public Result doIt() {
+    if (redoCommand() == false) {
+      return new Result(false, null, editMsgIfHaveOverdue(ERR_FAIL), goku
+          .getTaskList().getAllIncomplete());
+    } else {
+      return new Result(true, editMsgIfHaveOverdue(String.format(MSG_SUCCESS,
+          goku.getRedoInputList().pollLast())), null, goku.getTaskList()
+          .getAllIncomplete());
+    }
+  }
+
+  public void addToUndoList(String input) {
+    TaskList currList = new TaskList();
+    currList = list.clone();
+
+    goku.getUndoList().offer(currList);
+    goku.getUndoInputList().offer(input);
+  }
+
+  public boolean redoCommand() {
+    if (goku.getRedoList().isEmpty() || goku.getRedoInputList().isEmpty()) {
+      return isEmpty;
+    }
+
+    addToUndoList(goku.getRedoInputList().getLast());
+    TaskList prevList = goku.getRedoList().pollLast();
+    goku.setTaskList(prevList);
+
+    return !isEmpty;
+  }
+}
---
+++ src/goku/util/InvalidDateRangeException.java
@@ -0,0 +1,10 @@
+package goku.util;
+
+//@author A0096444X
+@SuppressWarnings("serial")
+public class InvalidDateRangeException extends Exception {
+  
+  public InvalidDateRangeException(String message) {
+      super(message);
+  }
+}
